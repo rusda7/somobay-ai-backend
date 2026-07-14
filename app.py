2e5e35c281c1eea2718fc43d49d6e20f07906653
rusda7 <rajislam19@gmail.com>
2026-07-14 23:55:25 +0600

    ok

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 # ChromaDB এর জন্য নতুন ইম্পোর্ট
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 load_dotenv()
 
